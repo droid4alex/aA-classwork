dec86b266cdd3ec3595e473a334d96aaaa3bb05d
Reed Haubenstock <rhaubenstock@users.noreply.github.com>
2020-12-04 14:45:09 -0800

    Finished KnightsTravails.rb Part 1

--- W03D5/KnightsTravails.rb
+++ W03D5/KnightsTravails.rb
@@ -1,7 +1,8 @@
 require_relative "./skeleton/lib/00_tree_node.rb"
 
 class KnightPathFinder
-    
+    attr_reader :root_node
+
     def initialize(pos)
         @root_node = PolyTreeNode.new(pos)
         @considered_positions = [pos]
@@ -18,9 +19,33 @@ class KnightPathFinder
     end
 
     def new_move_positions(pos)
-        new_positions = KnightPathFinder.valid_moves(pos).reject{|el| @considered_positions.include?(el)}
+        # new_positions = KnightPathFinder.valid_moves(pos).reject{|el| @considered_positions.include?(el)}
+        new_positions = []
+        KnightPathFinder.valid_moves(pos).each do |el|
+            # if el is in @considered_positions -> do not shovel in 
+            new_positions << el if !@considered_positions.include?(el)
+        end
+
         @considered_positions += new_positions
         new_positions
     end
 
+    def build_move_tree
+        # create new PolyTreeNodes for each new position
+        # @root <- children to root if reachable within one move
+
+        node_queue = [@root_node]
+        until node_queue.empty?
+            node = node_queue.shift
+            # get new moves
+            new_moves = new_move_positions(node.value)
+            # create PolyTreeNode for each new move
+            new_moves.each do |move|
+                new_node = PolyTreeNode.new(move)
+                node_queue << new_node
+                new_node.parent = node
+            end
+            # make sure order is correct
+        end
+    end
 end
