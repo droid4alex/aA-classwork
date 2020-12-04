2eadbf5d49a3099c64702a5740c198bf54c25f12
Reed Haubenstock <rhaubenstock@users.noreply.github.com>
2020-12-04 11:50:30 -0800

    Worked on 00_tree_node.rb for 15 minutes

--- W03D5/skeleton/lib/00_tree_node.rb
+++ W03D5/skeleton/lib/00_tree_node.rb
@@ -1,3 +1,25 @@
 class PolyTreeNode
 
+  attr_reader :children, :parent, :value
+
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  def parent=(parent_node)
+    #parent: parent_node
+    #child: self
+
+    if parent_node == nil
+      @parent = nil
+      return nil
+    end
+    @parent.children.delete(self) if @parent != nil
+    parent_node.children << self if !parent_node.children.include?(self)
+    @parent = parent_node
+  end
+
+  
 end
