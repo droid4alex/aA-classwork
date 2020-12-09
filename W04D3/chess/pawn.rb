a879643161ea3ba7022c6d6557c2df4ac2a8b80d
bcdguz <bcdguz96@gmail.com>
2020-12-09 14:53:19 -0800

    Worked on pawn.rb for our chess game

--- W04D3/chess/pawn.rb
+++ W04D3/chess/pawn.rb
@@ -5,6 +5,7 @@ class Pawn < Piece
   def moves
     row, col = @pos
     #white pawn move [1,0] ... black pawn move [-1,0]
+    #bottom 3 functions to move; forward direction, forward step, side attack
  
   end
 
@@ -27,8 +28,30 @@ class Pawn < Piece
   end
 
   def forward_steps
+    arr = []
+    row, col = @pos
+    if at_start_row?
+      arr << [row+forward_dir, col] if @rows[row+forward_dir][col] == @null_piece 
+    else  
+      return 1
+    end
+
 
   end
 
+  def side_attacks
+    row, col = pos
+    arr = []
+
+    if self.color == :white
+      arr << [row+1,col+1] if @rows[row+1][col+1] != @null_piece && @rows[row+1][col+1].color != :white
+      arr << [row+1,col-1] if @rows[row+1][col-1] != @null_piece && @rows[row+1][col-1].color != :white
+    else  
+      arr << [row-1,col+1] if @rows[row-1][col+1] != @null_piece && @rows[row-1][col+1].color != :black
+      arr << [row-1,col-1] if @rows[row-1][col-1] != @null_piece && @rows[row-1][col-1].color != :black
+    end
+
+    arr
+  end
 
 end
