11d7f2c5605ac2f7c75905d0f22cd467b5b17f2a
droid4alex <droid4alex@gmail.com>
2020-12-17 14:44:54 -0800

    Worked on associations for W05D4

--- W05D4/skeleton/app/models/enrollment.rb
+++ W05D4/skeleton/app/models/enrollment.rb
@@ -1,2 +1,21 @@
 class Enrollment < ApplicationRecord
+
+
+  #start with foreign keys, do belongs_to first
+
+  #enrollments is like a joins table
+
+  #always start with belongs_to, then locate where the correseponging has_many will be in (course.rb for this)
+
+  #seed database using commands in lecture, then go into console to test like we did in lecture
+
+  belongs_to :student,
+    primary_key: :id,
+    foreign_key: :student_id,
+    class_name: :User
+
+  belongs_to :course,
+    primary_key: :id,
+    foreign_key: :course_id,
+    class_name: :Course
 end
