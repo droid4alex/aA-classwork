adea7a2d88814d9239d94bc9621ebf582ce9ed04
colewendling <colewendling@utexas.edu>
2020-12-16 19:00:41 -0600

    setup initial ruby file with SQL classes

--- W05D3/aa_questions/questions_database.rb
+++ W05D3/aa_questions/questions_database.rb
@@ -11,6 +11,83 @@ class QuestionsDBConnection < SQLite3::Database
     end
 end
 
-class Questions
-    
+
+
+class Users 
+
+    attr_reader :id, :fname, :lname
+
+    def initialize(options)
+        @id = options['id']
+        @fname = options['fname']
+        @lname = options['lname']
+    end
+
+    def find_by_id(id)
+
+        QuestionsDBConnection.instance.execute(<<-SQL, self.id, self.fname, self.lname)
+
+         SELECT
+             users.*
+         FROM
+             users
+         WHERE
+             users.id = self.id
+        SQL
+
+    end
+
+    def find_by_name
+    end
+
 end
+
+class Questions
+
+    def initialize
+    end
+
+    def find_by_id
+    end
+
+    def find_by_title
+    end
+
+    def find_by_author_id
+    end
+
+end
+
+class QuestionsFollows
+
+    def initialize
+    end
+
+end
+
+class Replies
+
+    def initialize
+    end
+
+    def find_by_id
+    end
+
+    def find_by_user_id
+    end
+
+    def find_by_question_id
+    end
+
+end
+
+class QuestionLikes
+
+    def initialize
+    end
+
+    def find_by_id
+    end
+
+end
+
