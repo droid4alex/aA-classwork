dad693d6a8a786a355a0db2594e63e56338d6532
AlexeyS <xengawe@gmail.com>
2020-12-17 16:04:40 -0800

    add some associations

--- W05D4/skeleton/app/models/course.rb
+++ W05D4/skeleton/app/models/course.rb
@@ -1,2 +1,33 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id            :bigint           not null, primary key
+#  name          :string
+#  prereq_id     :integer
+#  instructor_id :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
 class Course < ApplicationRecord
+
+  belongs_to :student, #bananable 
+    primary_key: :id,
+    foreign_key: :student_id,
+    class_name: :User
+
+
+  has_many :enrolled_students, #bananable
+    primary_key: :id,
+    foreign_key: :student_id,
+    class_name: :User 
+
+  has_many :enrollments,
+    primary_key: :id,
+    foreign_key: :student_id,
+    class_name: :Enrollment
+
+  has_many :enrollments, 
+    through: :enrollments,
+    source: :student 
 end
--- W05D4/skeleton/app/models/user.rb
+++ W05D4/skeleton/app/models/user.rb
@@ -1,2 +1,27 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class User < ApplicationRecord
+
+  #enrolled_courses
+  has_many :enrolled_courses,
+    primary_key: :id,
+    foreign_key: :course_id,
+    class_name: :Course 
+
+  has_many :enrollments,
+    primary_key: :id,
+    foreign_key: :student_id,
+    class_name: :Enrollment
+
+  has_many :enrollments, 
+    through: :enrollments, 
+    source: :course 
+
 end
