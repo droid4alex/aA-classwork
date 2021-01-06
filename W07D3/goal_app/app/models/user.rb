4a68246d608ee593da6512faf3202183a7c0f8d4
shinara03 <shinara03@gmail.com>
2021-01-06 15:41:00 -0800

    Add methods to user rb file

--- W07D3/goal_app/app/models/user.rb
+++ W07D3/goal_app/app/models/user.rb
@@ -1,4 +1,38 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, length: {minimum: 6}, allow_nil: true
+
+  after_initialize :ensure_session_token
+  attr_reader :password
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+
+    if user && user.is_password?(password)
+      user
+    else
+      nil
+    end
+  end
+
+  def password=(pw)
+    self.password_digest = BCrypt::Password.create(pw)
+    @password = pw
+  end
   
+  def is_password?(pw)
+    BCrpyt::Password.new(self.password_digest).is_password?(pw)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom::urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom::urlsafe_base64
+  end
+
 end
