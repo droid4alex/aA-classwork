0ae1f265a085188a79bcde05f4b9e5b92d17b272
appkenny <kzeng.95@gmail.com>
2021-01-13 14:17:19 -0800

    completed phase 1

--- W08D3/skeleton/phase_1_arrays.js
+++ W08D3/skeleton/phase_1_arrays.js
@@ -13,7 +13,31 @@ Array.prototype.uniq = function() {
 // console.log([1, 2, 2, 3, 3, 3].uniq()) // => [1, 2, 3])
 
 Array.prototype.twoSum = function() {
+  let arr = [];
 
+  for (i = 0; i < this.length; i++) {
+    for (j = i; j < this.length; j++) {
+      if (i !== j && this[i] + this[j] === 0) {
+        arr.push(i, j);
+      }
+    }
+  }
+
+  return arr;
 }
 
-console.log([1, 5, 4, -5].twoSum()) // => [1, 3])
+// console.log([1, 5, 4, -5].twoSum()) // => [1, 3])
+
+Array.prototype.transpose = function() {
+  let arr = [];
+  for (i = 0; i < this.length; i++) {
+    let row = [];
+    for (j = 0; j < this.length; j++) {
+      row.push(this[j][i])
+    }
+    arr.push(row);
+  }
+  return arr;
+}
+arr2 = [[1,2],["a","b"]].transpose()
+console.log(arr2)
