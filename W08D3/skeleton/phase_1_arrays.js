3a93142df3de83dc21146af7ae85157881fc0691
droid4alex <droid4alex@gmail.com>
2021-01-13 13:52:33 -0800

    Started phase_1_arrays.js

--- W08D3/skeleton/phase_1_arrays.js
+++ W08D3/skeleton/phase_1_arrays.js
@@ -1,1 +1,19 @@
-console.log(1)
+Array.prototype.uniq = function() {
+  let arr = [];
+  for (let i = 0; i < this.length; i++){
+    if (arr.includes(this[i])){
+      continue;
+    } else {
+      arr.push(this[i]);
+    }
+  }
+  return arr;
+}
+
+// console.log([1, 2, 2, 3, 3, 3].uniq()) // => [1, 2, 3])
+
+Array.prototype.twoSum = function() {
+
+}
+
+console.log([1, 5, 4, -5].twoSum()) // => [1, 3])
