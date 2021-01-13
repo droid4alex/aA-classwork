271110be2fd9bb625c1958300fe7c0386af41fde
droid4alex <droid4alex@gmail.com>
2021-01-13 15:39:46 -0800

    Worked on phase 2

--- W08D3/skeleton/phase_2_enumerable.js
+++ W08D3/skeleton/phase_2_enumerable.js
@@ -2,9 +2,9 @@
 
 // arr = [1, 2, 3]
 
-// let cb = function(el) {
-//   console.log(el)
-// }
+let cb = function(el) {
+  return el*2;
+}
 
 // function myFunc(theObject) {
 //   console.log(theObject);
@@ -18,11 +18,15 @@ Array.prototype.myEach = function(cb) {
 }
 
 Array.prototype.myMap = function(cb) {
-  let arr = [];
+  let arr = this;
   // this.myEach(arr.push(cb))  
-  arr.push(cb(this.myEach))
+  // arr.push(cb(this.myEach))
+  function doSomething(){
+    arr = arr.myEach(cb)
+  }
+  doSomething();
   return arr;
 }
 
-arr = [1, 2, 3];
-arr.myMap(console.log);
+let arr = [1, 2, 3];
+console.log(arr.myMap(cb));
