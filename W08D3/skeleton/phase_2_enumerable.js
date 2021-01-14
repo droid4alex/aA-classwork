06f0114f3e71f0e2c69ba44ecfa61d458cef4975
appkenny <kzeng.95@gmail.com>
2021-01-13 16:52:15 -0800

    finished myMap

--- W08D3/skeleton/phase_2_enumerable.js
+++ W08D3/skeleton/phase_2_enumerable.js
@@ -2,31 +2,37 @@
 
 // arr = [1, 2, 3]
 
-let cb = function(el) {
-  return el*2;
-}
 
 // function myFunc(theObject) {
-//   console.log(theObject);
-// }
-//Zach's example... cb and myFunct are the same
+  //   console.log(theObject);
+  // }
+  //Zach's example... cb and myFunct are the same
+  let cb = function(el) {
+    return el*2;
+  }
 Array.prototype.myEach = function(cb) {
   for (i=0; i<this.length; i++){
-    cb(i)
+    cb(this[i])
     // cb(this[i])
   }
 }
 
 Array.prototype.myMap = function(cb) {
-  let arr = this;
-  // this.myEach(arr.push(cb))  
-  // arr.push(cb(this.myEach))
-  function doSomething(){
-    arr = arr.myEach(cb)
-  }
-  doSomething();
+  let arr = [];
+
+  // function asdf(el) {
+
+  //   arr.push(cb(el))
+  // }
+
+  // this.myEach(asdf)
+  this.myEach( el =>  arr.push(cb(el)) )
   return arr;
 }
 
 let arr = [1, 2, 3];
 console.log(arr.myMap(cb));
+
+Array.prototype.myReduce = function(cb,) {
+
+}
