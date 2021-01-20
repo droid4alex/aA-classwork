77fbb973c879a2e0c7e024a729942851988acacd
droid4alex <droid4alex@gmail.com>
2021-01-20 11:26:27 -0800

    Added instructions

--- W09D3/jQuery Tic Tac Toe/src/index.js
+++ W09D3/jQuery Tic Tac Toe/src/index.js
@@ -4,3 +4,11 @@ const Game = // require appropriate file
   $(() => {
     // Your code here
   });
+
+// You shouldn't have to change anything in the Tic Tac Node solution but read and refer to it!
+
+// Complete the require statements in src / index.js to require ttt - view and the game as dependencies. When webpack processes index.js(the entry file), it will include all required files within main.js(the output file).
+
+// Webpack will output the bundled file as dist / main.js, for which we have included a script tag in index.html.
+
+// Open a new terminal tab or window and run webpack--watch--mode = development.The--watch flag directs webpack to re - bundle your app whenever you save a bundled file.
