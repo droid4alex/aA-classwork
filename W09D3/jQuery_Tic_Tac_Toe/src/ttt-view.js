9a597008ebb357658c2f26f6a5fe400db8605900
droid4alex <droid4alex@gmail.com>
2021-01-20 17:00:25 -0800

    Finished tic tac toe

--- W09D3/jQuery_Tic_Tac_Toe/src/ttt-view.js
+++ W09D3/jQuery_Tic_Tac_Toe/src/ttt-view.js
@@ -14,20 +14,38 @@ class View {
   }
 
   makeMove($square) {
-    $square.attr('id', 'id1');
-    let div = document.getElementById('id1');
-    let pos = div.getBoundingClientRect();
-    alert("Coordinates: " + pos.left + "px, " + pos.top + "px");
+    let str = $square.attr('id').slice(2);
+    let arr = str.split("");
+    let x = parseInt(arr[0]);
+    let y = parseInt(arr[1]);
+    let pos = [x, y];
+  
     this.game.playMove(pos);
-    
+    // alert(this.game.currentPlayer);
     $square.addClass('selected');
+ // $('.bucket-list').html('<p>wassuuuuuup</p>')
+  // $('.bucket-list').text('hi')
+    if (this.game.currentPlayer === 'o'){
+      $square.addClass('o');
+      $('.o').html('O')
+    }
+    else {
+      $square.addClass('x');
+      $('.x').html('X')
+    }
+    if (this.game.isOver()){
+      alert('Its over');
+    }
+    
   }
 
   setupBoard() {
     const $ul = $('<ul></ul>');
     for (let i = 0; i < 3; i++){
       for (let j = 0; j < 3; j++) {
-        const $li = $('<li class="col'+i+'"></li>');
+        let str = i.toString() + j.toString();
+        // console.log(str);
+        const $li = $('<li id="li' + str + '"></li>');
         $ul.append($li);
       }
     }
